cee1d6a348ec1d45d4171460ffc2f97f5b47a46f
James Williams <me@jamesrwilliams.co.uk>
2015-03-08 13:57:57 +0000

    WIP

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,9 @@ var i, player, story, settings;
 		
 	    console.log(device.cordova);
 	    $("#device").html("Device: " + device.model);
+	    checkConnection();
+	    
+	    navigator.geolocation.getCurrentPosition(onSuccess, onError);
 	
 	}
 	
@@ -47,6 +50,30 @@ var i, player, story, settings;
 	    
 	    $("#network").html("Network: " + states[networkState]);
 	}
+	
+	// onSuccess Callback
+	// This method accepts a Position object, which contains the
+	// current GPS coordinates
+	//
+	var onSuccess = function(position) {
+	    alert('Latitude: '          + position.coords.latitude          + '\n' +
+	          'Longitude: '         + position.coords.longitude         + '\n' +
+	          'Altitude: '          + position.coords.altitude          + '\n' +
+	          'Accuracy: '          + position.coords.accuracy          + '\n' +
+	          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+	          'Heading: '           + position.coords.heading           + '\n' +
+	          'Speed: '             + position.coords.speed             + '\n' +
+	          'Timestamp: '         + position.timestamp                + '\n');
+	};
+	
+	// onError Callback receives a PositionError object
+	//
+	function onError(error) {
+	    alert('code: '    + error.code    + '\n' +
+	          'message: ' + error.message + '\n');
+	}
+	
+	
 	
 	/*
 		===========
