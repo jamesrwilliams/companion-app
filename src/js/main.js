953e3624803fa37d00dd0ea8d23f1fa095b6a692
James Williams <me@jamesrwilliams.co.uk>
2015-03-08 15:16:24 +0000

    Added Splashscreens

--- src/js/main.js
+++ src/js/main.js
@@ -58,11 +58,6 @@ var i, player, story, settings;
 	var onSuccess = function(position) {
 	    alert('Latitude: '          + position.coords.latitude          + '\n' +
 	          'Longitude: '         + position.coords.longitude         + '\n' +
-	          'Altitude: '          + position.coords.altitude          + '\n' +
-	          'Accuracy: '          + position.coords.accuracy          + '\n' +
-	          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-	          'Heading: '           + position.coords.heading           + '\n' +
-	          'Speed: '             + position.coords.speed             + '\n' +
 	          'Timestamp: '         + position.timestamp                + '\n');
 	};
 	
@@ -72,8 +67,6 @@ var i, player, story, settings;
 	    alert('code: '    + error.code    + '\n' +
 	          'message: ' + error.message + '\n');
 	}
-	
-	
 	
 	/*
 		===========
