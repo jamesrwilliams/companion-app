6c0da68dfb2a01389add0e112d8d1d02ec3b4d6c
James Williams <me@jamesrwilliams.co.uk>
2015-03-25 13:35:35 +0000

    Custom map market

--- src/js/main.js
+++ src/js/main.js
@@ -38,6 +38,12 @@ var i, player, story, settings, Hammer, device, lore;
 				    }
 			
 			map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+			
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(position.coords.latitude,position.coords.longitude),
+				map: map,
+				title: 'Hello World!'
+			});
 	
 	};
 	
